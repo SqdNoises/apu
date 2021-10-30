fdc2cafa3d8329e2c7c227be77136bbaaf2e7c25
iVacon <71210872+iVacon@users.noreply.github.com>
2021-10-30 17:23:56 +0300

    remove testing token

--- environ.py
+++
@@ -1,5 +0,0 @@
-import os
-def token():
-    print('Setting bot token to TOKEN variable from environ.py ...')
-    os.environ["TOKEN"] = "OTAzMTYwODc0MzcyNTc1MjMy.YXo8HA.D7ADM_5qOIhtIfGZ6TQt7-CbICI"
-    print('Done.')
