09f4d0d4d3681c27b3d217c8b260f41ddc816bd3
iVacontry <HelloWorld791@protonmail.com>
2021-10-30 17:23:01 +0300

    Remove my testing token ;-;

--- environ.py
+++ environ.py
@@ -1,5 +1,5 @@
 import os
 def token():
     print('Setting bot token to TOKEN variable from environ.py ...')
-    os.environ["TOKEN"] = "OTAzMTYwODc0MzcyNTc1MjMy.YXo8HA.ERAy1CSyQUN9GBBE5TrKBsf4NhA"
+    os.environ["TOKEN"] = "OTAzMTYwODc0MzcyNTc1MjMy.YXo8HA.D7ADM_5qOIhtIfGZ6TQt7-CbICI"
     print('Done.')
